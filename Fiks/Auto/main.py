dabbebcef641254d1fb2b92640321c40a21e7d45
Leqier <triksiand@gmail.com>
2019-10-14 21:18:59 +0200

    funkcni ale trva to moc dlouho

--- Fiks/Auto/main.py
+++ Fiks/Auto/main.py
@@ -1,37 +1,21 @@
 from queue import Queue
-maxX = 10
-maxY = 8
-q = Queue()
-Silnice = [[[0,0,0,0] for r in range(maxX)] for s in range(maxY)]
-XYcil = [5,0]
-XYstart = [0,0]
-Silnice[XYcil[1]][XYcil[0]] = [-1,-1,-1,-1] #-1 for cil
-Silnice[XYstart[1]][XYcil[0]] = [1,1,1,1] #1 for start
 
-d = {
-    "End":False,
-    "newDirection":0,
-    "state":1,
-    "Silnice": Silnice,
-    "newXYauto": XYstart
-}
 # direction == 0 == ^
 # direction == 1 == >
 # direction == 2 == v
 # direction == 3 == <
 
+
 def findPath():
     while True:
         p = q.get()
         if p["End"]:
-            for y in p["Silnice"]:
-                print(str(y))
-            return p["state"]
+            return int(p["state"])-1
         TurnRight(p["newXYauto"], p["newDirection"], p["state"], p["Silnice"])
         Streight(p["newXYauto"], p["newDirection"], p["state"], p["Silnice"])
         q.task_done()
-
-
+        if q.empty():
+            return "No solution"
 
 
 def TurnRight(XYauto, direction, state, Silnice):
@@ -58,8 +42,8 @@ def TurnRight(XYauto, direction, state, Silnice):
 
         return False
     pristiPoloha = Silnice[newXYauto[1]][newXYauto[0]][newDirection]
-    print("pristi poloha: "+str(pristiPoloha))
-    if ((pristiPoloha < state+1) & (pristiPoloha != -1) & (pristiPoloha != 0)) | (pristiPoloha == -2):
+
+    if ((pristiPoloha < state+1) & ((pristiPoloha != -1) & (pristiPoloha != 0))) | (pristiPoloha == -2):
         return False
 
     if pristiPoloha == -1:
@@ -97,14 +81,10 @@ def Streight(XYauto, direction, state, Silnice):
         return False
 
     pristiPoloha = Silnice[newXYauto[1]][newXYauto[0]][newDirection]
-    print("pristi poloha: " + str(pristiPoloha))
-    if ((pristiPoloha < state+1) & (pristiPoloha != -1) & (pristiPoloha != 0)) | (pristiPoloha == -2):
+    if ((pristiPoloha < state+1) & ((pristiPoloha != -1) & (pristiPoloha != 0))) | (pristiPoloha == -2):
         return False
     if pristiPoloha == -1:
         End = True
-
-
-
 
     Silnice[newXYauto[1]][newXYauto[0]][newDirection] = state + 1
     d = {
@@ -117,6 +97,44 @@ def Streight(XYauto, direction, state, Silnice):
     q.put(d)
     return d
 
+maxX = 10
+maxY = 8
 
-q.put(d)
-print(str(findPath()))
+
+XYcil = [5,5]
+XYstart = [0,0]
+
+
+f = open("input.txt")
+pocet_zadani = f.readline()
+vysledky = []
+for z in range(int(pocet_zadani)):
+    q = Queue()
+    maxX, maxY, K = f.readline().split()
+    maxX = int(maxX)
+    maxY = int(maxY)
+    Xstart,Ystart, Xcil, Ycil = f.readline().split()
+    XYstart = [int(Xstart)-1, int(Ystart)-1]
+    XYcil = [int(Xcil)-1, int(Ycil)-1]
+    Silnice = [[[0, 0, 0, 0] for r in range(int(maxX))] for s in range(int(maxY))]
+    Silnice[XYcil[1]][XYcil[0]] = [-1, -1, -1, -1]  # -1 for cil
+    Silnice[XYstart[1]][XYstart[0]] = [1, 1, 1, 1]  # 1 for start
+    for k in range(int(K)):
+        x,y = f.readline().split()
+        Silnice[int(y)-1][int(x)-1] = [-2, -2, -2, -2]
+
+    d = {
+        "End": False,
+        "newDirection": 0,
+        "state": 1,
+        "Silnice": Silnice,
+        "newXYauto": XYstart
+    }
+    q.put(d)
+    vysledky.append(str(findPath()))
+
+w = open("output.txt","w")
+for i, v in enumerate(vysledky):
+    w.write(v)
+    if i < int(pocet_zadani)-1:
+        w.write("\n")
